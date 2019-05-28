e692a40e53d82c5b1ed5e47e80ae113ea937bcb7
MfDevNet <MfDevNet@gmail.com>
2019-05-29 00:00:17 +0430

    Stream code Complete

--- main/java/HW10/stramproject/Project.java
+++ main/java/HW10/stramproject/Project.java
@@ -1,5 +1,6 @@
 package HW10.stramproject;
 
+import javax.persistence.ManyToOne;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -9,25 +10,31 @@ import static java.util.stream.Collectors.groupingBy;
 
 public class Project {
     public static void main(String[] args) {
-        String[] name={"Amir", "Hatef", "Mehran", "Mojtaba", "Mohammad", "Ali",
+        String[] name = {"Amir", "Hatef", "Mehran", "Mojtaba", "Mohammad", "Ali",
                 "Davood", "Reza", "Mohsen"};
 
-        List<String> strings =Arrays.asList(name);
 
+        // use array
+        Map<Integer, List<String>> re = Arrays.stream(name).collect(groupingBy(String::length));
+        System.out.println(re);
 
+        // convert array to list
+
+        List<String> strings = Arrays.asList(name);
         Map<Integer, List<String>> nameGrouping = strings.stream()
                 .collect(groupingBy(String::length));
 
-
-
         System.out.println(nameGrouping);
 
+
+        //use array
+        Map<Integer, Long> countArray = Arrays.stream(name)
+                .collect(groupingBy(String::length, counting()));
+        System.out.println(countArray);
+        // use list
         Map<Integer, Long> count = strings.stream()
                 .collect(groupingBy(String::length, counting()));
-
         System.out.println(count);
-
-
 
 
     }
